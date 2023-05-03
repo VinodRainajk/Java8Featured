3306fc1d0898b8697b647bcba581ee7d5872639a
vinodrainak <vinod.rainajk@gmail.com>
2023-05-03 22:52:11 +0530

    Java8Practice

--- src/main/java/Consumer/ConsumerTest.java
+++ src/main/java/Consumer/ConsumerTest.java
@@ -1,13 +1,28 @@
 package Consumer;
 
+import Student.Student;
+import Student.StudentDataBase;
+import java.util.List;
 import java.util.Locale;
 import java.util.function.Consumer;
 
 public class ConsumerTest {
 
+
+    public static void printall()
+    {
+        Consumer<Student> consumerStudent =  (student)-> System.out.println(student);
+        List<Student> studentList =  StudentDataBase.getAllStudents();
+        studentList.forEach(consumerStudent);
+
+    }
+
     public static void main(String[] args) {
 
         Consumer<String> consumer = (S)->  System.out.println(S.toUpperCase());
         consumer.accept("Vinod");
+        System.out.println("***** Second example *******");
+        printall();
+
     }
 }
