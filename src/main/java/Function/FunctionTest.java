18da3123524be883c60b488fd13cb8b0fe4829aa
vinodrainak <vinod.rainajk@gmail.com>
2023-05-05 13:28:23 +0530

    Java8Practice

--- src/main/java/Function/FunctionTest.java
+++ src/main/java/Function/FunctionTest.java
@@ -1,23 +1,50 @@
 package Function;
 
+import Student.Student;
 import Student.StudentDataBase;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FunctionTest {
 
+    static Predicate<Student> GPACriteria = (stdnt) -> stdnt.getGpa() > 3.5;
+
+    static BiFunction<List<Student>, Predicate<Student>, Map<String, Double>> highGPA =
+
+            (StudentList, studentgpa) -> {
+                Map<String, Double> studntMap = new HashMap<>();
+                StudentList.forEach(student -> {
+                    if (studentgpa.test(student)) {
+                        studntMap.put(student.getName(), student.getGpa());
+                    }
+                });
+                return studntMap;
+            };
+
+
     public static void main(String[] args) {
 
-        Function<String,Integer> functionoutput = (student) -> student.length();
+        Function<String, Integer> functionoutput = (student) -> student.length();
 
         StudentDataBase.getAllStudents().forEach((student) ->
-        {
-            System.out.println("Name of Student is "+student.getName() +" Length of Name is "+
-                    functionoutput.apply(student.getName()));
-        }
+                {
+                    System.out.println("Name of Student is " + student.getName() + " Length of Name is " +
+                            functionoutput.apply(student.getName()));
+                }
 
         );
 
-    }
 
+
+        System.out.println( highGPA.apply(StudentDataBase.getAllStudents(),GPACriteria));
+
+
+
+    }
 }
